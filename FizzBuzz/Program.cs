7ea6aa054eb42eab98e1c5ea9fe569777479f274
Jasmin Dzanic <jasmin@dzanic.se>
2025-05-09 18:45:16 +0200

    Update Program.cs

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
@@ -11,15 +11,22 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            // FizzBuzz implementation in C#
+            // Prints numbers from 1 to N (max 100), replacing divisible ones with Fizz, Buzz, or FizzBuzz
+
             Console.WriteLine("We are playing FizzBuzz! (From 1 to N or up to 100)");
             Console.WriteLine("\nIf divisible by X: Fizz \nif divisible by Y: Buzz \nIf divisible by both X and Y: FizzBuzz\n");
+
+            // Ask user for input values
             Console.Write("Enter the numbers X, Y and N seperated with blankspace: ");
-            
+
+            // Read and parse input values
             string[] xynNumbers = Console.ReadLine().Split();
             int x = int.Parse(xynNumbers[0]);
             int y = int.Parse(xynNumbers[1]);
             int n = int.Parse(xynNumbers[2]);
 
+            // Loop from 1 to N (but not above 100)
             for (int i = 1; i <= n && i <= 100; i++) 
             {
                 if (i % x == 0 && i % y == 0)
@@ -31,6 +38,8 @@ namespace FizzBuzz
                 else
                     Console.WriteLine(i);
             }
+
+            // Wait for user to press Enter before closing
             Console.ReadLine();
         }
     }
